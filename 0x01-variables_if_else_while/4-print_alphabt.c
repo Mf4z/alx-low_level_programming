f210c7e7c05d76f16d47e587409b278ca7b46553
Marwan Mai <emef4z@gmail.com>
2022-10-14 15:39:25 -0700

    Add 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+  * main - Code entry point
+  *
+  * Return: Return 0 after execution
+  */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' || alphabet == 'q')
+			continue;
+
+		putchar(alphabet);
+
+		alphabet++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
