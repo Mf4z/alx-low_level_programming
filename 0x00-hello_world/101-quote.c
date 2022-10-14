44fe87b3a8a0d1e53002e8b67e2f9664b332352b
Marwan Mai <emef4z@gmail.com>
2022-10-13 21:46:30 -0700

    Add 101-qoute.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Decription : Print without using printf or puts
+ * Return: Always 0 (Success)
+ */
+
+char main(void)
+{
+	char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-1
+		9\n";
+
+	return (qoute);
+}
