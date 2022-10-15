cfd94986bd0431f13502255a61ff45072eede5bf
Marwan Mai <emef4z@gmail.com>
2022-10-14 20:28:00 -0700

    Add task 100-print_combo3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+  * main - Code entry point
+  *
+  * Return: Return 0 after execution
+  */
+int main(void)
+{
+	int k = '0';
+
+	for (int i = '0'; i <= '9'; i++)
+	{
+		for (int j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+
+			if (i == j)
+				continue;
+
+			if (i == '8' && j == '9')
+				break;
+
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		k++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
