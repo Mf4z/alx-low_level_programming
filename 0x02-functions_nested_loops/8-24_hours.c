7edb4037b533541fa6c27589f6d288ad19b74dcb
Marwan Mai <emef4z@gmail.com>
2022-10-19 20:49:37 -0700

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints clock digits in hours:minutes
+  *
+  * Description: Prints out time in 24hour format hours:minutes
+  */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+}
