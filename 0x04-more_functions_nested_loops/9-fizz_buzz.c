db001c0571edb7bc37cd2666f1352eaa5cc337b0
Marwan Mai <emef4z@gmail.com>
2022-10-20 20:54:43 -0700

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - check the code
+  *
+  * Description: Prints out FizzBuzz for Multiples of
+  * 5 and 3, Fizz for multiples of 3, Buzz for Multiples
+  * of 5
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", i);
+	}
+
+	return (0);
+}
