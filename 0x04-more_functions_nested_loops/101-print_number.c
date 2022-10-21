06ba846d973e058e9f912fc71e49ad78c591645a
Marwan Mai <emef4z@gmail.com>
2022-10-20 22:10:30 -0700

    Add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_number - Prints number
+  * @n: number passed in for check
+  */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar('0' + (num % 10);)
+
+}
