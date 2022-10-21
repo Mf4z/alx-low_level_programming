7f12de84879364b9a3a4f2f49b00b6cecdb7f54f
Marwan Mai <emef4z@gmail.com>
2022-10-20 18:44:33 -0700

    Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_line - Prints n number of lines
+  *
+  * @n: number passed to determine number of lines
+  */
+void print_line(int n)
+{
+	while (n <= 0)
+	{
+		_putchar('_');
+		i--;
+	}
+
+	_putchar('\n');
+}
