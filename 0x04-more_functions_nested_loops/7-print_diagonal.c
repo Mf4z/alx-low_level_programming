889e6c3df717eb8cb3277af1fd4206d0f0e53889
Marwan Mai <emef4z@gmail.com>
2022-10-20 19:54:02 -0700

    Add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Prints diagonal \ n number of times
+  *
+  * @n: number passed to determine number of lines
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (n > 0)
+	{
+		j = i;
+
+		while (j > 0)
+		{
+			_putchar(' ');
+			j--;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+		n--;
+	}
+
+	if (i < 1)
+		_putchar('\n');
+}
