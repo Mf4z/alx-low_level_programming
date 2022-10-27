8f90f23314f6eb9c07a9144d734b52f40dee89a9
Marwan Mai <emef4z@gmail.com>
2022-10-26 17:13:32 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,14 +17,19 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while (src[i] != '\0' && i < n)
+	while ((src[i] != '\0') && (i < n))
 	{
 		dest[i] = src[i];
 
 		i++;
 	}
 
-	dest[i] = '\0';
+	while (n > i)
+	{
+		dest[i] = '\0';
+
+		i++;
+	}
 
 	return (dest);
 }
