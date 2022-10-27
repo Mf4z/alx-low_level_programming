49effa2326d0a7900079013171551f1dc11befae
Marwan Mai <emef4z@gmail.com>
2022-10-27 10:14:11 -0700

    Correct 103-infinite_add.c v4

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * *infinite_add - function that adds two numbers
   *
@@ -16,25 +15,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (n1[len_a] != '\0')
 		len_a++;
-
 	while (n2[len_b] != '\0')
 		len_b++;
-
 	if (len_a > len_b)
 		largest = len_a;
 	else
 		largest = len_b;
-
 	if ((largest + 1) >= size_r)
 		return (0);
-
 	r[largest + 1] = '\0';
 
 	while (largest >= 0)
 	{
 		a = (n1[len_a - 1] - '0');
 		b = (n1[len_b - 1] - '0');
-
 		if (len_a > 0 && len_b > 0)
 			sum = a + b + carry;
 		else if (len_a < 0 && len_b > 0)
@@ -59,7 +53,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		len_b--;
 		largest--;
 	}
-
 	if (*(r) != 0)
 		return (r);
 	else
