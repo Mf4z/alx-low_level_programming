f82fcaf86360a9bf02e3c43fee7d9c11eaf31133
Marwan Mai <emef4z@gmail.com>
2022-10-27 16:23:46 -0700

    Update 103-infinite_add.c v2

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,33 @@
 #include "main.h"
+
 /**
-  * *infinite_add - function that adds two numbers
+  * reverse_string- function that reverses the content
+  * of an array of integers
   *
+  * @n: a pointer to chat array
+  *
+  * Return: Nothing
+  */
+void reverse_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char holder;
+
+	while (n[i] != '\0')
+		i++;
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		holder = n[j];
+		n[j] = n[i];
+		n[i] = holder;
+	}
+}
+
+/**
+  *infinite_add - function that adds two numbers
   * @n1: a pointer to the first number
   * @n2: a pointer to the second number
   * @r: a pointer to the buffer
@@ -11,50 +37,46 @@
   */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len_a = 0, len_b = 0, carry = 0, a, b, sum, largest;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int num1 = 0, num2 = 0, temp_sum = 0;
 
-	while (n1[len_a] != '\0')
-		len_a++;
-	while (n2[len_b] != '\0')
-		len_b++;
-	if (len_a > len_b)
-		largest = len_a;
-	else
-		largest = len_b;
-	if ((largest + 1) >= size_r)
+	while (n1[i] != '\0')
+		i++;
+
+	while (n2[j] != '\0')
+		j++;
+
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	r[largest + 1] = '\0';
-
-	while (largest >= 0)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		a = (n1[len_a - 1] - '0');
-		b = (n1[len_b - 1] - '0');
-		if (len_a > 0 && len_b > 0)
-			sum = a + b + carry;
-		else if (len_a < 0 && len_b > 0)
-			sum = b + carry;
-		else if (len_a > 0 && len_b < 0)
-			sum = a + carry;
+		if (i < 0)
+			num1 = 0;
 		else
-			sum = carry;
+			num1 = n1[i] - '0';
+		if (j < 0)
+			num2 = 0;
+		else
+			num2 = n2[j] - '0';
 
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
+		temp_sum = num1 + num2 + overflow;
+		if (temp_sum >= 10)
+			overflow = 1;
 		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[largest] = sum;
-		len_a--;
-		len_b--;
-		largest--;
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_sum % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	reverse_string(r);
+	return (r);
 }
+
