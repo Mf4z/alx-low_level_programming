46b0bd06a46f8cf21a1cc66ae80732861eaf31f2
Marwan Mai <emef4z@gmail.com>
2022-11-01 09:37:40 -0700

    Add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  * _strspn - function that locates a character in a string
+  *
+  * @s: a char pointer
+  * @accept: prefix string
+  *
+  * Return: Returns the number of bytes in the initial segment of s
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count;
+
+	i = 0;
+	count = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[i])
+			{
+				count++;
+				break;
+			}
+
+			j++;
+		}
+
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+
+	return (count);
+}
