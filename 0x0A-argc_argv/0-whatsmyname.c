9dec99625ef2ab07eba24c63820d9668498b60d9
Marwan Mai <emef4z@gmail.com>
2022-11-04 13:46:44 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
   * @argv: Commands passed to the main function through commandline
   * Return: Always 0.
   */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
