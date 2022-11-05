187a60c2f946e8fb0b9fe40709ede9ec671d0665
Marwan Mai <emef4z@gmail.com>
2022-11-04 17:35:22 -0700

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,34 +4,32 @@
 #include "main.h"
 
 /**
-  * main - program that multiplies two numbers
+  * main - program that adds positive numbers
   * @argc: Count of arguments passed to main function
   * @argv: Commands passed to the main function through commandline
   * Return: Always 0.
   */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc < 3)
-		printf("0\n");
-
-	else if (argc > 2)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (isdigit(argv[i]))
-				sum += atoi(argv[i]);
-			else
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
+
 				return (1);
 			}
 		}
 
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
+
 	}
 
+	printf("%d\n", sum);
 
 	return (0);
 }
