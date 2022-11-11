c8366dcb869b047747232593b1d8e590675661e8
Marwan Mai <emef4z@gmail.com>
2022-11-10 18:35:47 -0800

    Correct 2-calloc.c v1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	unsigned int i;
 
-	if (nmemb < 1 || size < 1)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(sizeof(nmemb * size));
