221974f8e11f4bc46445731b00c77d161c893040
Marwan Mai <emef4z@gmail.com>
2022-11-11 06:23:48 -0800

    Correct 1-string_nconcat.c v11

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,40 +12,35 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_length, s2_length, i, offset;
 	char *concat;
+
+	unsigned int len = n, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	s1_length = 0;
-	s2_length = 0;
+	for (i = 0; s1[i]; i++)
+		len++;
 
-	while (*(s1 + s1_length) != '\0')
-		s1_length++;
 
-	while (*(s2 + s2_length) != '\0')
-		s2_length++;
-
-	/* Check if n is >= s2_length */
-	if (n >= s2_length)
-		offset = s2_length;
-	else
-		offset = n;
-
-	concat = malloc(sizeof(*concat) * (s1_length + offset + 1));
+	concat = malloc(sizeof(*concat) * (len + 1));
 
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; i < s1_length; i++)
-		*(concat + i) = *(s1 + i);
-	for (i = s1_length; i < (s1_length + offset); i++)
-		*(concat + i) = *(s2 + i - s1_length);
+
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		concat[len++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		concat[len++] = s2[i];
 
 	/* Add '/0' if it is the end of s2 */
-	*(concat + i) = '\0';
+	concat[len] = '\0';
 
 	return (concat);
 }
