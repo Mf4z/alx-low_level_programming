5bba3edc96351cfd3cb2fbdf55b3265a995ddaad
Marwan Mai <emef4z@gmail.com>
2022-11-10 21:35:18 -0800

    Correct 1-string_nconcat.c v9

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,10 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s1_length = 0;
 	s2_length = 0;
 
-	while (s1[s1_length] != '\0')
+	while (*(s1 + s1_length) != '\0')
 		s1_length++;
 
-	while (s2[s2_length] != '\0')
+	while (*(s2 + s2_length) != '\0')
 		s2_length++;
 
 	/* Check if n is >= s2_length */
@@ -42,12 +42,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (concat == NULL)
 		return (NULL);
 	for (i = 0; i < s1_length; i++)
-		concat[i] = s1[i];
+		*(concat + i) = *(s1 + i);
 	for (i = s1_length; i < concat_length; i++)
-		concat[i] = s2[i - s1_length];
+		*(concat + i) = *(s2 + i - s1_length);
 
 	/* Add '/0' if it is the end of s2 */
-	concat[i] = '\0';
+	*(concat + i) = '\0';
 
 	return (concat);
 }
