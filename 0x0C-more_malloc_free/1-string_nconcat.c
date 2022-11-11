b023c5769e82a6945572ef148dd2990b6cd866b0
Marwan Mai <emef4z@gmail.com>
2022-11-10 17:12:38 -0800

    Add 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * *str_nconcat - function that concatenates two strings which contains s1
+  * *string_nconcat - function that concatenates two strings which contains s1
   * followed by the first n bytes of s2, and null terminated
   * @s1: string literal passed
   * @s2: string literal passed
@@ -12,12 +12,11 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int concat_length, s1_length, s2_length, i, j;
+	unsigned int concat_length, s1_length, s2_length, i;
 	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
@@ -32,30 +31,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	/* Add 1 to make it length of array not string and to include '/0' */
 	s2_length++;
-
 	/* Check if n is >= s2_length */
 	if (n >= s2_length)
 		n = s2_length;
-
-	/* concat_length = s1_length + s2_length; */
 	concat_length = s1_length + n;
 
 	concat = malloc(sizeof(*concat) * concat_length);
 
 	if (concat == NULL)
 		return (NULL);
-
 	/* Fill up concat with s1 string */
 	for (i = 0; i < s1_length; i++)
 		concat[i] = s1[i];
-
 	/* Continue filling up concat with s2 string */
-	for (j = 0; j < n; j++)
-		concat[s1_length + j] = s2[j];
+	for (i = 0; i < n; i++)
+		concat[s1_length + i] = s2[i];
 
 	/* Add '/0' if it is the end of s2 */
-	if (s2[j] != '\0')
-		s2[j + 1] = '\0';
+	if (s2[i] != '\0')
+		s2[i + 1] = '\0';
 
 	return (concat);
 }
