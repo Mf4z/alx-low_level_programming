a1ab9621e4ff93ad287b9de45fff55e32848246b
Marwan Mai <emef4z@gmail.com>
2022-11-24 15:04:23 -0800

    Add 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+  * print_list - function that prints all the elements of a list_t list
+  *
+  * @h: pointer to node structure
+  *
+  * Return: Returns the number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	size_t numOfElems = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+
+		printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+
+		numOfElems++;
+	}
+
+	return (numOfElemes)
+}
