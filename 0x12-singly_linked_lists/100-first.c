c1d31f47636054a37a387d5bfaeab02ab4f995a8
Marwan Mai <emef4z@gmail.com>
2022-11-24 16:48:36 -0800

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * pre_main - function that executes before main
+  *
+  * Return: Returns the number of nodes
+  */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
